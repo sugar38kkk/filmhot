af78f7da7acc0d9c48e5c9667f05e12664e70004
Sugar <hnquang.20it9@vku.udn.vn>
2022-10-07 02:29:42 +0700

    update

--- src/services/client.ts
+++ src/services/client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const client = axios.create({
   baseURL: "https://ga-mobile-api.loklok.tv/cms/app",
   headers: {
-    lang: "vi",
+    lang: "en",
     versioncode: "11",
     clienttype: "ios_jike_default",
   },
